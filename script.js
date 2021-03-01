2d06d50a66c4829ee6c46fa9481c5058829e5466
AustinJoo97 <austinjoo97@gmail.com>
2021-03-01 15:06:10 -0500

    coded generatingPassword function to create a concatenated string using Math functions that randomly select and join 15 characters together to form a password

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
+
 // Added character array to be utilized when generating a password using "randomly" chosen characters
 let charArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "?", ":", ";", "{", "}", "^", "|", "~", "_"]
+
+// Coded generatePassword function to create a concatenated string using Math functions that randomly select and join 15 characters together to form a "random" password
+function generatePassword(){
+  let tempPassword;
+
+  for(let i= 0; i < 15; i++){
+    let nextChar = charArray[Math.floor(Math.random * charArray.length)]
+    tempPassword.concat(nextChar);
+    console.log(tempPassword);
+  }
+
+  return tempPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -9,8 +23,9 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
