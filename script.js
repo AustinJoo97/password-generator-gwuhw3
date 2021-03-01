0652cad936d8b0f119f0acfcbdca03396708a941
AustinJoo97 <austinjoo97@gmail.com>
2021-03-01 15:47:05 -0500

    created a new html button that is initially hidden using css. upon clicking the generate password button (indicating a password was successfully generated and rendered to the viewport), the new copy button would be visibile (style change done using js) and allow the user to copy the value of of the 'textarea' html element (where the pw can be exists) to the clipboard to be saved elsewhere

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
+let copyBtn = document.querySelector("#copy")
 
 // Added character array to be utilized when generating a password using "randomly" chosen characters
 let charArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "?", ":", ";", "{", "}", "^", "|", "~", "_"]
+
 
 // Coded generatePassword function to create a concatenated string using Math functions that randomly select and join 15 characters together to form a "random" password
 function generatePassword(){
@@ -13,10 +15,10 @@ function generatePassword(){
     charSelector = Math.floor(Math.random() * charArray.length)
     tempPassword += charArray[charSelector];
   }
-  console.log(tempPassword);
 
   return tempPassword;
 }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -26,7 +28,25 @@ function writePassword() {
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Added two more functions:
+  // The first will, after generating a password by clicking the "id = generate" button, change the display of a hidden button called "id = copy"
+  // The second function allows the "id = copy" button to take information from the DOM using the getElementById method to select the generated password and copy it to the clipboard in case the user would like to save it elsewhere. 
+function enabler() {
+  document.querySelector('#copy').style.display = "inline-block";
+}
+
+function copier(){
+  document.getElementById("password").select();
+  document.execCommand("copy");
+  alert(`Copied PW: ${document.getElementById("password").value}`);
+}
+
+generateBtn.addEventListener("click", enabler);
+
+copyBtn.addEventListener("click", copier);
 
 
