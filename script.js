1c35a516569eba62de2e9d57c1c91f4debcde24e
AustinJoo97 <austinjoo97@gmail.com>
2021-03-01 16:21:49 -0500

    added necessary functionality via an array which will determine what character types can be utilized in the generated password and conditionals dictating whether certain 'randomly' selected characters can be used

--- script.js
+++ script.js
@@ -2,22 +2,38 @@
 let generateBtn = document.querySelector("#generate");
 let copyBtn = document.querySelector("#copy")
 
-// Added character object to be utilized when generating a password using "randomly" chosen characters
+// Added character object to be utilized when generating a password using "randomly" chosen characters; number keys correspond to different options when user is prompted
 let charOptions = {
-  numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  symbols: ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "?", ":", ";", "{", "}", "^", "|", "~", "_"]
+  0: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  1: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  2: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  3: ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "?", ":", ";", "{", "}", "^", "|", "~", "_"]
 }
 
 // Coded generatePassword function to create a concatenated string using Math functions that randomly select and join 15 characters together to form a "random" password
 function generatePassword(){
+  // Several prompts will first be given to the user to dictate the length and criteria of the desired password. This will dictate both the initial for loop to determine acceptedOptions as well as the latter for loop when the actual password is generated.
+
+  // This array will dictate what the criteria the user has chosen and will determine which characters can be used in the password generated. Each criterium will correspond to a value between 0 and 3.
+  let acceptedOptions = [];
+  for(let key in charOptions){
+    if(charOptions[key]){
+      acceptedOptions.push(key);
+    }
+  }
   let tempPassword = "";
+  let typeSelector;
   let charSelector;
 
+  // This for loop will first calculate a value within the length of Object.keys(charOptions) [this was hardcoded for now] and only if the "random" value is within the accpedOptions array will the for loop continue to add a character to the password. Otherwise, the loop will decrement and be rerun.
   for(let i= 0; i < 15; i++){
-    charSelector = Math.floor(Math.random() * charArray.length)
-    tempPassword += charArray[charSelector];
+    typeSelector = Math.floor(Math.random() * 4);
+    if(typeSelector in acceptedOptions){
+      charSelector = Math.floor(Math.random() * charOptions[typeSelector].length);
+      tempPassword += charOptions[typeSelector][charSelector];
+    } else {
+      i -= 1;
+    }
   }
 
   return tempPassword;
