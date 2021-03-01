45ea4615279ee696f7be541da2b739af6e38a64e
AustinJoo97 <austinjoo97@gmail.com>
2021-03-01 15:00:25 -0500

    commented unknown/unnecessary script tags in html file and added charArray in js file to be utilized when generating a 'random' password

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+// Added character array to be utilized when generating a password using "randomly" chosen characters
+let charArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", "=", ">", "?", ":", ";", "{", "}", "^", "|", "~", "_"]
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
