152a256119b71dd78c003e4ddb1386f090c0808a
AustinJoo97 <austinjoo97@gmail.com>
2021-03-02 19:45:21 -0500

    added multiple confirm popups that will add values to the allowedOptions array to generate a password with the desired criteria; if no proper criteria is chosen, the page will alert the user and ask that a new password criteria be selected

--- script.js
+++ script.js
@@ -12,13 +12,14 @@ let charOptions = {
 
 // Coded generatePassword function to create a concatenated string using Math functions that randomly select and join 15 characters together to form a "random" password
 function generatePassword(){
-  // Several prompts will first be given to the user to dictate the length and criteria of the desired password. This will dictate both the initial for loop to determine acceptedOptions as well as the latter for loop when the actual password is generated.
+  // Several prompts will first be given to the user to dictate the length and criteria of the desired password. This will dictate both the initial for-loop to determine acceptedOptions as well as the latter for loop when the actual password is generated.
 
-  // The first prompt will determine the length of the password. If the password desired is too long (exceeds 128 characters), the password is invalid and will have no need to ask for criteria. This is why they promopt was chosen to be first.
+  // The first prompt will determine the length of the password. If the password desired is too long (exceeds 128 characters) or not a number, the password is invalid and will have no need to ask for criteria. This is why they promopt was chosen to be first.
   // A while loop was used to reassign and check if pwLength was an acceptable value to continue the user to enter a correct value until an acceptable value was entered.
     // If the prompt is cancelled, the code discontinues and no password is generated
   // The Number() function was used as it parses a string into either an interger or a float value and returns non numerical strings as NaN
-  let pwLength = prompt(`Please enter desired password length up to 128:`)
+  let pwLength = prompt(`Please enter desired password length up to 128:`);
+
   if(pwLength){
     while (!Number(pwLength) || Number(pwLength) > 128){
       pwLength = prompt(`Invalid. Please enter a number up to 128: `)
@@ -26,23 +27,40 @@ function generatePassword(){
         break;
       }
     }
+
+    let allowNumber = confirm("Would you like numbers in your password?");
+    let allowLower = confirm("Would you like lower case letters in your password?");
+    let allowUpper = confirm("Would you like upper case letters in your password?");
+    let allowSymbols = confirm("Would you like symbols in your password?");
+
     // This array will dictate what the criteria the user has chosen and will determine which characters can be used in the password generated. Each criterium will correspond to a value between 0 and 3.
-    let acceptedOptions = [0, 2];
-    for(let key in charOptions){
-      if(charOptions[key]){
-        acceptedOptions.push(key);
-      }
+    let acceptedOptions = [];
+    if(allowNumber){
+      acceptedOptions.push(0);
+    } 
+    if(allowLower){
+      acceptedOptions.push(1);
+    }
+    if(allowUpper){
+      acceptedOptions.push(2);
+    }
+    if(allowSymbols){
+      acceptedOptions.push(3);
+    }
+
+    if(acceptedOptions.length === 0){
+      alert("Could not generate valid password. Please regenerate password with valid criteria!")
+      return;
     }
 
     let tempPassword = "";
     let typeSelector;
     let charSelector;
   
-    // This for loop will iterate the number of times as determined by the user in the initial prompts asking for the desired password length. With this number, a "random" number will be chosen between 0-3 (index values corresponding to the charOptions obj) and take acceptable characters and add them to the password string.
+    // This for loop will iterate the number of times as determined by the user in the initial prompt asking for the desired password length. With this number, a "random" number will be chosen between 0-3 (index values corresponding to the charOptions obj) and take acceptable characters, adding them to the password string given the generated "typeSelector" exists in the "acceptedOptions" array.
     for(let i= 0; i < pwLength; i++){
       typeSelector = Math.floor(Math.random() * 4);
-      if(acceptedOptions[typeSelector]){
-      // if(typeSelector in acceptedOptions){
+      if(acceptedOptions.includes(typeSelector)){
         charSelector = Math.floor(Math.random() * charOptions[typeSelector].length);
         tempPassword += charOptions[typeSelector][charSelector];
       } else {
@@ -78,8 +96,7 @@ function enabler() {
 function copier(){
   document.getElementById("password").select();
   document.execCommand("copy");
-  alert(`Copied secure password: ${document.getElementById("password").value}
-  Please save elsewhere for later use!`);
+  alert(`Copied secure password: ${document.getElementById("password").value} \nPlease save elsewhere for later use!`);
 }
 
 generateBtn.addEventListener("click", enabler);
